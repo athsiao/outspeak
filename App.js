3796ca518296ffec40d4798282dbf0f86555d7ca
Athena Siaotong <asiaotong@ucmerced.edu>
2020-10-03 04:43:24 -0700

    Changed navigation to bottom tab navigator

--- App.js
+++ App.js
@@ -1,43 +1,87 @@
 import * as React from 'react';
 import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-const Stack = createStackNavigator();
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+        <AppContainer />
+    );
+  }
 }
 
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate('Home')}
-      />
-    </View>
-  );
+class HomeScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> This is my Home screen </Text>
+      </View>
+    );
+  }
 }
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+class PollsScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> This is my Polls screen </Text>
+      </View>
+    );
+  }
 }
 
-export default App;
+class SearchScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> This is my Search screen </Text>
+      </View>
+    );
+  }
+}
+
+class ContactsScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> This is my Contacts screen </Text>
+      </View>
+    );
+  }
+}
+
+const bottomTabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        title: 'Home'
+      }
+    },
+    Polls: {
+      screen: PollsScreen,
+      navigationOptions: {
+        title: 'Polls'
+      }
+    },
+    Search: {
+      screen: SearchScreen,
+      navigationOptions: {
+        title: 'Search'
+      }
+    },
+    Contacts: {
+      screen: ContactsScreen,
+      navigationOptions: {
+        title: 'Contacts'
+      }
+    }
+  },
+
+  {
+    initialRouteName: 'Home'
+  }
+);
+
+const AppContainer = createAppContainer(bottomTabNavigator);
