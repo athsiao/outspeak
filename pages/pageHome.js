0138b15d01c72b576b48b97bee697120e6d7e2a9
Shivanshu Gupta <shivanshu.gupta@gmail.com>
2020-10-03 15:56:58 -0700

    button add

--- pages/pageHome.js
+++ pages/pageHome.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Image, View, Text, TouchableOpacity } from 'react-native';
+import { Image, View, Text, TouchableOpacity,Button } from 'react-native';
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 
@@ -7,7 +7,7 @@ class HomeScreen extends React.Component {
 
     render() {
       return(
-        
+
         // home screen content
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
           <Text> This is my Home screen </Text>
@@ -17,20 +17,6 @@ class HomeScreen extends React.Component {
     }
   }
 
-class SettingsButton extends React.Component {
-
-    HomeNavigator.setOptions({
-      headerRight: () => (
-        <Button onPress={() => alert('This is a button!')}
-        title="Info"
-        color="#fff" />
-      ),
-    });
-  }, [HomeNavigator]);
-  
-  return <Text>Count: {count}</Text>;
-}
-
 // navigation
 export const HomeNavigator = createAppContainer(
   createStackNavigator({
@@ -38,6 +24,12 @@ export const HomeNavigator = createAppContainer(
       screen: HomeScreen,
       navigationOptions: {
         headerTitle: 'Home',
+        headerRight: () => (
+            <Button
+              onPress={() => alert('This is a button!')}
+              title="Info"
+              color="#fff"
+            />),
       }
     }
   })
